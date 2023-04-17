61886a2e2442e79b9503e19590ba23bd7b804ed4
Neville Kadwa <neville@kadwa.com>
2023-04-17 10:09:46 -0400

    Update retry - 1.0.2

--- retry.rb
+++ retry.rb
@@ -1,8 +1,8 @@
 class Retry < Formula
   desc "Command retry for the shell"
   homepage "https://github.com/kadwanev/retry/"
-  url "https://github.com/kadwanev/retry/releases/download/1.0.1/retry-1.0.1.tar.gz"
-  sha256 "3df0d341e7973be0752b468e5d84643559ecbeb40e959c0b2ffbc2fa42616e1c"
+  url "https://github.com/kadwanev/retry/releases/download/1.0.2/retry-1.0.2.tar.gz"
+  sha256 "3c4cdf33142a6ea7e1bf31224a6da5573c77ba9436c6af29f29fadf870de9d28"
 
   depends_on "gnu-getopt"
 
